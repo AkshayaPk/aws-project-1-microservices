8442e9f0365b1ddfb2e090ff86f355136ea0acee
Akshay Pk <akshaypk@Akshays-MacBook-Pro-2.local>
2020-07-22 19:23:38 +0530

    Added CORS

--- src/main/java/com/example/student/StudentController.java
+++ src/main/java/com/example/student/StudentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController 
-
+@CrossOrigin("*")
 public class StudentController {
 
 	@Autowired
